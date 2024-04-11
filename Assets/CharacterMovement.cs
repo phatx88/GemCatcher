7eee31e3f0d311711b580f276d581b4e87aa5b29
Phat Luong <phat.x.luong@gmail.com>
2024-04-11 22:08:06 +0700

    ~ add single jump feature

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float jumpForce = 7f;
+    private bool isGrounded;
+
+    private Rigidbody2D rb;
     private Animator animator;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>(); //start animation
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,6 +31,22 @@ public class CharacterMovement : MonoBehaviour
         {
             transform.position += new Vector3(moveHorizontal * speed * Time.deltaTime, 0f, 0f);
         }
+        //kiem tra co input jump cua nguoi dung va character dang o trang thai grounded
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false; //set isgrounded to prevent double jump
+        }
 
+
+    }
+
+    //ham kiem tra va set character vao trang thai tram dat
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
